6b6f4b5d69c0d379a5f46502f21c839c89ba2767
Jeppe Johansen <jj@whyyy.dk>
2019-08-01 11:17:17 +0200

    Members and sponsors work

--- pages/members/index.vue
+++ pages/members/index.vue
@@ -1,14 +1,87 @@
 <template>
-  <v-container fluid>
-    <v-layout column justify-center align-center>
-      <v-flex xs12 sm8 md6>
+  <v-container v-if="membersByCountry" grid-list-lg fluid>
+    <v-combobox
+      multiple
+      v-model="selectedCountries"
+      :items="countries"
+      chips
+      label="Select countries"
+    >
+      <template v-slot:selection="data">
+        <v-chip
+          :key="JSON.stringify(data.item.name)"
+          v-bind="data.attrs"
+          :input-value="data.selected"
+          :disabled="data.disabled"
+          @click.stop="data.parent.selectedIndex = data.index"
+          @click:close="data.parent.selectItem(data.item.name)"
+        >
+          <v-avatar left>
+            <img :src="data.item+'.png'" height="25px" />
+          </v-avatar>
+          {{ data.item }}
+        </v-chip>
+      </template>
+    </v-combobox>
+    <v-layout wrap>
+      <v-flex v-for="member in membersByCountry" :key="id" xs2>
+        <v-card color="#152e63" raised dark min-height="300px" light>
+          <v-img contain :src="member.logoURL" height="200px"></v-img>
+          <v-card-text>
+            <span class="black--text nowrap">{{member.name}}</span>
+            <br />
+            <span class="text--primary">
+              <a :href="member.websiteURL">Website</a>
+            </span>
+            <span>
+              <v-img
+                :key="name"
+                position="left"
+                v-for="country in member.countries"
+                contain
+                :src="country.name+'.png'"
+                height="25px"
+              ></v-img>
+            </span>
+          </v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import members from "~/graphql/members.gql";
 export default {
-  components: {}
+  apollo: {
+    members: {
+      query: members
+    }
+  },
+  data() {
+    return {
+      countries: ["denmark", "sweden", "norway", "finland"],
+      selectedCountries: ["denmark", "sweden", "norway", "finland"]
+    };
+  },
+  computed: {
+    membersByCountry() {
+      if (this.members != undefined) {
+        return this.members.filter(x => {
+          let valid = true;
+          x.countries.forEach(country => {
+            valid = this.selectedCountries.includes(country.name);
+          });
+          return valid;
+        });
+      }
+    }
+  }
 };
 </script>
+
+<style scoped>
+.nowrap {
+  white-space: nowrap;
+}
+</style>
--- pages/sponsors/index.vue
+++ pages/sponsors/index.vue
@@ -1,31 +1,50 @@
 <template>
   <v-container grid-list-lg fluid>
+    <v-combobox
+      multiple
+      v-model="selectedCountries"
+      :items="countries"
+      chips
+      label="Select countries"
+    >
+      <template v-slot:selection="data">
+        <v-chip
+          :key="JSON.stringify(data.item.name)"
+          v-bind="data.attrs"
+          :input-value="data.selected"
+          :disabled="data.disabled"
+          @click.stop="data.parent.selectedIndex = data.index"
+          @click:close="data.parent.selectItem(data.item.name)"
+        >
+          <v-avatar left>
+            <img :src="data.item+'.png'" height="25px" />
+          </v-avatar>
+          {{ data.item }}
+        </v-chip>
+      </template>
+    </v-combobox>
     <v-layout wrap>
-      <v-flex v-for="sponsor in sponsors" :key="id" xs2>
-        <v-card light raised v-if="sponsor.venue != null">
-          <v-img contain :src="sponsor.venue.logoURL" height="200px"></v-img>
+      <v-flex v-for="sponsor in sponsorsByCountry" :key="id" xs2>
+        <v-card color="#152e63" min-height="300px" raised dark>
+          <v-img contain :src="sponsor.logoURL" height="200px"></v-img>
           <v-card-text>
-            <span class="black--text">{{sponsor.venue.name}}</span>
+            <span class="nowrap">{{sponsor.name}}</span>
             <br />
             <span class="text--primary">
-              <span class="grey--text">Venue Sponsor</span>
-              <br />
-              <a :href="sponsor.venue.websiteURL">Website</a>
+              <a :href="sponsor.websiteURL">Website</a>
+            </span>
+            <span>
+              <v-img
+                :key="name"
+                position="left"
+                v-for="country in sponsor.countries"
+                contain
+                :src="country.name+'.png'"
+                height="25px"
+              ></v-img>
             </span>
           </v-card-text>
         </v-card>
-        <!-- <v-card light raised v-else-if="sponsor.other">
-          <v-img contain :src="sponsor.venue.logoURL" height="200px"></v-img>
-          <v-card-text>
-            <span class="black--text">{{sponsor.venue.name}}</span>
-            <br />
-            <span class="text--primary">
-              <span class="grey--text">Other Sponsor</span>
-              <br />
-              <a :href="sponsor.venue.websiteURL">Website</a>
-            </span>
-          </v-card-text>
-        </v-card> -->
       </v-flex>
     </v-layout>
   </v-container>
@@ -38,6 +57,31 @@ export default {
     sponsors: {
       query: sponsors
     }
+  },
+  data() {
+    return {
+      countries: ["denmark", "sweden", "norway", "finland"],
+      selectedCountries: ["denmark", "sweden", "norway", "finland"]
+    };
+  },
+  computed: {
+    sponsorsByCountry() {
+      if (this.sponsors != undefined) {
+        return this.sponsors.filter(x => {
+          let valid = true;
+          x.countries.forEach(country => {
+            valid = this.selectedCountries.includes(country.name);
+          });
+          return valid;
+        });
+      }
+    }
   }
 };
 </script>
+
+<style scoped>
+.nowrap {
+  white-space: nowrap;
+}
+</style>
