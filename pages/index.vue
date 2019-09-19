47bf78ab96f145f6153e9a7ce6d2382fc9a78502
Kasper Nissen <kaspernissen@gmail.com>
2019-09-19 20:12:44 +0200

    Initial play with the frontpage styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,42 @@
 <template>
+  <section style="color: white;">
+  <v-parallax
+    dark
+    src="cover2.png"
+  >
+    <v-row align="center" justify="center">
+      <h1 class="mt-12">Cloud Native Nordics</h1>
+    </v-row>
+    <v-row align="center" justify="center">
+      <h3>Join our slack community</h3>
+    </v-row>
+    <v-row align="center" justify="center">
+        <v-text-field
+          color="#FFFFFF"
+          label="Email"
+          outlined
+          append-icon="mdi-arrow-right-circle"
+          v-model="email"
+          @click:append="sendInvite"
+        ></v-text-field>
+
+      <v-col md="6" offset-md="3">
+      <v-snackbar color="primary" multi-line timeout="20000" v-model="snackbar">
+        <span v-html="response"></span>
+        <v-btn text @click="snackbar = false">Close</v-btn>
+      </v-snackbar>
+      </v-col>
+      </v-row>
+
+  </v-parallax>
   <v-container grid-list-md text-center fill-height>
     <v-layout align-center justify-space-around column>
-      <v-img class="d-none d-sm-flex" width="25%" src="snowflake.png" />
-      <v-img class="d-flex d-sm-none" width="50%" src="snowflake.png" />
       <h1 class="mt-5">Cloud Native Nordics</h1>
-
       <br>
       <h3>Join our slack community</h3>
       <v-flex xs-12 mr-5>
         <v-text-field
+          color="#FFFFFF"
           label="Email"
           outlined
           append-icon="mdi-arrow-right-circle"
@@ -20,36 +48,9 @@
         <span v-html="response"></span>
         <v-btn text @click="snackbar = false">Close</v-btn>
       </v-snackbar>
-
-      <v-layout align-center justify-center row fill-height>
-        <v-flex xs-12 mr-5>
-          <v-btn
-            class="custom-btn"
-            href="https://github.com/cloud-native-nordics"
-            target="_blank"
-            icon
-            min-height="120"
-            min-width="120"
-          >
-            <v-img src="github.png" class="primary-overlay" />
-          </v-btn>
-        </v-flex>
-
-        <v-flex xs-12 ml-5>
-          <v-btn
-            class="custom-btn"
-            href="https://www.cloudnativenordics.com"
-            target="_blank"
-            icon
-            min-height="120"
-            min-width="120"
-          >
-            <v-img src="slack.png" class="primary-overlay" />
-          </v-btn>
-        </v-flex>
-      </v-layout>
     </v-layout>
   </v-container>
+  </section>
 </template>
 
 <script>
