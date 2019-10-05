795388f2721d50c64bd7ac2079fd8c87d98c6752
Rasmus Steiniche <rasmus@steiniche.net>
2019-10-05 19:49:00 +0200

    Added digital ocean logo to index page

--- pages/index.vue
+++ pages/index.vue
@@ -45,10 +45,14 @@
         <h2>Some other stuff</h2>
       </v-col>
     </v-row>
+
     <br>
-
+    <v-row>
+      <v-col md=12 justify="center" align="center">
+        <span"><p style="color: #502366;">Proudly running on </p><a href="https://www.digitalocean.com/"><img width="200px" src="DO_Logo_horizontal_blue.svg" /></a></span>
+      </v-col>
+    </v-row>
   </section>
-
 </template>
 
 <script>
