8c501dc6881c38d8d8f0e359ab21015f997a8677
Kasper Nissen <kaspernissen@gmail.com>
2019-10-22 11:59:05 +0200

    Remove members and insert empty page for events

---
+++ pages/events/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <v-container grid-list-lg fluid>
+      <v-layout row fill-height class="pt-5 pb-5 d-flex flex-wrap flex-md-nowrap">
+        <headline text="events"></headline>
+      </v-layout>
+
+    </v-container>
+  </section>
+</template>
+
+<script>
+import events from "~/graphql/events.gql";
+import Headline from "~/components/common/Headline.vue";
+export default {
+  components: {
+    Headline: Headline
+  },
+};
+</script>
+
+<style scoped>
+.nowrap {
+  white-space: nowrap;
+}
+</style>
