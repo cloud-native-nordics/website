e960b0852319987819817e330d530adb1006f838
Kasper Nissen <kaspernissen@gmail.com>
2019-11-01 19:47:13 +0100

    Restyling section one and tweaking the layout of section two a bit (#43)

--- components/landing/SectionTwo.vue
+++ components/landing/SectionTwo.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container fluid class="section-two">
-    <v-row>
+    <v-row style="margin-bottom: -30px;">
       <v-spacer></v-spacer>
       <v-col sm="12" md="4" lg="4" cols="12">
-        <h3 class="d-none d-md-flex">Join our Slack community</h3>
+        <h3 class="d-none d-md-flex align-center">Join our Slack community</h3>
         <h3 class="d-block d-md-none d-sm-block text-center pl-0">Join our Slack community</h3>
       </v-col>
       <v-col sm="12" md="4" lg="4" cols="12">
