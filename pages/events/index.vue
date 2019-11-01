6ae0ea8cfe586bc2c16ea19ea294002c0b432e47
Kasper Nissen <kaspernissen@gmail.com>
2019-11-01 21:01:54 +0100

    Add initial data - styling is missing

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -4,18 +4,54 @@
       <v-layout row fill-height class="pt-5 pb-5 d-flex flex-wrap flex-md-nowrap">
         <headline text="events"></headline>
       </v-layout>
-
+      <v-layout wrap align-center>
+        <v-flex v-for="event in sortedMeetups" :key="event.id" xs12 lg4>
+          <v-card text>
+            <v-card-title>
+              <v-img
+                position="center"
+                contain
+                src="meetup.png"
+                height="250px"
+              ></v-img>
+            </v-card-title>
+            <v-card-text class="text-center font-weight-bold title">
+              {{ event.name }}
+            </v-card-text>
+            <v-card-actions>
+              <v-layout row fill-height>
+                <v-flex class="d-flex justify-center" xs12>
+                {{ event.date }}
+                </v-flex>
+              </v-layout>
+            </v-card-actions>
+            <br />
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </section>
 </template>
 
 <script>
-import events from "~/graphql/events.gql";
+import meetups from "~/graphql/events.gql";
 import Headline from "~/components/common/Headline.vue";
 export default {
   components: {
     Headline: Headline
   },
+  apollo: {
+    meetups: {
+      query: meetups
+    }
+  },
+  computed: {
+    sortedMeetups() {
+        return this.meetups.sort( ( a, b) => {
+            return new Date(b.date) - new Date(a.date);
+        });
+    }
+  }
 };
 </script>
 
