6f2414d8cdea69f66845c305b034d8e8e55f1c9e
Kasper Nissen <kaspernissen@gmail.com>
2019-11-07 21:00:50 +0100

    Change title of the page

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,8 +4,8 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: 'Cloud Native Nordics',
+    title: 'Cloud Native Nordics',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
