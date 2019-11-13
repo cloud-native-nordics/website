af83cb6c5e2038123dbc772f64c1df2d717ea936
Jeppe Johansen <jj@whyyy.dk>
2019-11-13 15:27:14 +0100

    Added presentation details page

---
+++ pages/presentations/_id.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container fluid v-if="presentation">
+    <v-row>
+      <v-col fill-height class="pt-5">
+        <headline :text="presentation.title"></headline>
+      </v-col>
+    </v-row>
+    <v-row class="pr-5 pl-5">
+      <v-col>
+        <b>Meetup:</b>
+        <span class="text--primary .text-no-wrap text-no-decoration">
+          <router-link
+            v-if="presentation.meetup.name"
+            :to="'/meetups/'+presentation.meetup.id"
+          >{{presentation.meetup.name}}</router-link>
+        </span>
+      </v-col>
+    </v-row>
+    <v-row class="pr-5 pl-5">
+      <v-col>
+        <b>Date:</b>
+        {{new Date(presentation.meetup.date).toLocaleDateString()}}
+      </v-col>
+    </v-row>
+    <v-row class="pr-5 pl-5">
+      <v-col>
+        <b>Duration:</b>
+        {{presentation.duration}}
+      </v-col>
+    </v-row>
+    <v-row class="pr-5 pl-5" v-if="presentation.slides">
+      <v-col>
+        <b>Slides:</b>
+        <a :href="presentation.slides" target="_blank">Link</a>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import presentation from "~/graphql/presentation.gql";
+import headline from "~/components/common/Headline";
+
+export default {
+  components: {
+    headline: headline
+  },
+  apollo: {
+    presentation: {
+      query: presentation,
+      variables() {
+        return { id: this.$route.params.id };
+      }
+    }
+  }
+};
+</script>
