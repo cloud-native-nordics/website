45e67a8c4373690e78f5fa65a300b87693d09b31
Nicolai Kobber <nkobber@gmail.com>
2019-11-14 17:11:39 +0100

    Fixed a typo

--- components/landing/SectionMap.vue
+++ components/landing/SectionMap.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col cols="2"></v-col>
       <v-col sm="12" md="12" lg="8" cols="12" justify="center" align="center">
-        <h2>Aliance across the nordic borders</h2>
+        <h2>Alliance across the nordic borders</h2>
         <p style="margin-bottom:40px; font-size: 20px;">Is your city missing on this map? We are very excited to help you to initiate a new meetup group!</p>
         <v-img contain max-width=80% src="/map.png" />
       </v-col>
