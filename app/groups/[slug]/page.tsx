166bb9026724f27659938b41058d36593b3337fc
Kasper Borg Nissen <kaspernissen@gmail.com>
2026-03-29 12:02:00 +0200

    style: place chapter logo above text instead of inline

--- app/groups/[slug]/page.tsx
+++ app/groups/[slug]/page.tsx
@@ -47,18 +47,16 @@ export default async function GroupPage({ params }: PageProps) {
             </svg>
             All Groups
           </Link>
-          <div className="flex items-center gap-4 mb-2">
-            {logoUrl && (
-              <img
-                src={logoUrl}
-                alt=""
-                className="w-16 h-16 rounded-xl object-cover"
-              />
-            )}
-            <h1 className="text-3xl sm:text-4xl font-heading font-extrabold text-white">
-              {group.name}
-            </h1>
-          </div>
+          {logoUrl && (
+            <img
+              src={logoUrl}
+              alt=""
+              className="w-16 h-16 rounded-xl object-cover mb-4"
+            />
+          )}
+          <h1 className="text-3xl sm:text-4xl font-heading font-extrabold text-white mb-2">
+            {group.name}
+          </h1>
           <p className="text-lg text-gray-300 mb-4">
             {group.city}, {group.country}
           </p>
